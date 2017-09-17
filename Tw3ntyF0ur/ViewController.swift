0a37e5eee35c1c2094fc1f54daff0df05476ca99
Sabrina Liu <sabrinaliu@Sabrinas-MacBook-Air.local>
2017-09-16 21:03:58 -0400

    part 2: keeps score for two players

--- Tw3ntyF0ur/ViewController.swift
+++ Tw3ntyF0ur/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lastUsr = 0
     @IBOutlet weak var num0: UILabel!
     @IBOutlet weak var num1: UILabel!
     @IBOutlet weak var num2: UILabel!
     @IBOutlet weak var num3: UILabel!
+    @IBOutlet weak var p1Score: UILabel!
+    @IBOutlet weak var p2Score: UILabel!
     @IBAction func dealButton(sender: AnyObject) {
+        if (lastUsr == 1){
+            p1Score.text = String(Int(p1Score.text!)! + 1)
+        }else if (lastUsr == 2){
+            p2Score.text = String(Int(p2Score.text!)! + 1)
+        }
+        lastUsr = 0
+        
         let cards:[UILabel] = [num0, num1, num2, num3]
         for c in cards{
             c.text = String(arc4random_uniform(13) + 1)
         }
         
+    }
+    
+    @IBAction func p1AnsButton(sender: AnyObject) {
+        if (lastUsr == 1){
+            lastUsr = 0
+        }else{
+            lastUsr = 1
+        }
+    }
+    @IBAction func p2AnsButton(sender: AnyObject) {
+        if (lastUsr == 2){
+            lastUsr = 0
+        }else{
+            lastUsr = 2
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
